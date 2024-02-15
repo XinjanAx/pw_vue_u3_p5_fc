846841d2c7d00873492b5e58229c587a95513f1d
AlexCondor <alexguerron@gmail.com>
2024-02-15 11:17:38 -0500

    CSS form estudiante GET y POST

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,50 +1,177 @@
 <template>
   <h1>Componente estudiante</h1>
-  <input v-model="id" type="text">
-  <button @click="consultarEstudiante()">Consultar</button>
-  <label>{{nombre}}</label>
-  <button @click="incertarEstudiante()">Incertar</button>
-  <input v-model="incertar" type="text">
+  <div class="contenedor">
+    <div id="insertar">
+        <input v-model="buscar" type="text">
+        <button @click="consultarEstudiante()">Consultar</button>
+    </div>
+
+    <form class="form" action="https://miapli.com/procesarEstudiante" method="post">
+            
+        <h2>Estudiente</h2>
+        <p type="Nombre:">{{estudiante.nombre}}</p>
+        <p type="Apellido">{{estudiante.apellido}}</p>
+        <p type="CI:">{{estudiante.cedula}}</p>
+        <p type="Genero:">{{estudiante.genero}}</p>
+        <p type="Fecha:">{{estudiante.fechaNacimiento}}</p>
+        <p type="Tipo Sangre:">{{estudiante.sangre}}</p>
+        <p type="Matricula:">{{estudiante.matricula}}</p>
+        <p type="Semestre:">{{estudiante.semestre}}</p>
+        <p type="Gratuidad:">{{estudiante.gratuidad}}</p>
+    </form>
+  </div>
+  <div class="contenedor">
+    <form class="form" action="https://miapli.com/procesarEstudiante" method="post">
+        <h2>Nuevo estudiante</h2>
+        <p type="Nombre:"><input v-model="nombre" type="text"></p>
+        <p type="Apellido"><input v-model="apellido" type="text"></p>
+        <p type="CI:"><input v-model="ci" type="text"></p>
+        <p type="Genero:"><input v-model="genero" type="text"></p>
+        <p type="Fecha:"><input v-model="fechaNacimiento" type="datetime-local"></p>
+        <p type="Tipo Sangre:"><input type="text"></p>
+        <p type="Matricula:"><input v-model="matricula" type="text"></p>
+        <p type="Semestre:"><input v-model="semestre" type="number"></p>
+        <p type="Gratuidad:"><input v-model="gratuidad" type="checkbox"></p>       
+    </form>
+    <button @click="insertarEstudiante()" class="btn">Guardar</button>
+  </div>
+  <div>
+
+  </div>
+  <!-- 
+  <div>
+    <input v-model="borrar" type="text">
+    <button @click="borrarEstudiante()">borrar</button>
+  </div>
+  -->
+  <div>
+
+  </div>
+  
+  
+
 </template>
 
 <script>
-import {consultarFachada,incertarFachada} from "../helpers/clienteEstudiante.js"
+import {consultarFachada,insertarFachada} from "../helpers/clienteEstudiante.js"
 export default {
     data(){
         return{
-            id:null,
+            buscar:null,
+            estudiante:{
+                id:null,
+                nombre: null,
+                apellido: null,
+                cedula: null,
+                genero: null,
+                fechaNacimiento: null,
+                sangre: null,
+                matricula: null,
+                semestre: null,
+                gratuidad: null
+            },
+
+
             nombre:null,
-            incertar:null
+            apellido:null,
+            ci:null,
+            genero:null,
+            fechaNacimiento:null,
+            TipoSangle:null,
+            matricula: null,
+            semestre: null,
+            gratuidad: null,
+
+            borrar:null,
         }
     },
     methods:{
         async consultarEstudiante(){
-            const data = await consultarFachada(this.id);
-            console.log('componente:');
+            const data = await consultarFachada(this.buscar);
+            console.log(this.borrar);
             console.log(data);
-            this.nombre = data.nombre;
-            console.log(this.nombre);
+            this.estudiante = data;
+            console.log(this.estudiante);
         },
-        async incertarEstudiante(){
-            const estudiante ={
-                nombre: "alex",
-                apellido: this.incertar,
-                cedula: "65858554+",
-                genero: "M",
-                fechaNacimiento: "2014-01-17T00:00:00",
-                sangre: "b+",
-                matricula: "pa-vig94614",
-                semestre: 7,
-                gratuidad: false
+        async insertarEstudiante(){
+            const incertar ={
+                nombre: this.nombre,
+                apellido: this.apellido,
+                cedula: this.cedula,
+                genero: this.genero,
+                fechaNacimiento: this.fechaNacimiento,
+                sangre: this.sangre,
+                matricula: this.matricula,
+                semestre: this.semestre,
+                gratuidad: this.gratuidad
             }
-
-           await incertarFachada(estudiante);
+           await insertarFachada(incertar);
+        },
+        async borrarEstudiante(){
+            await borrarFachada(this.borrar);
         }
+
     }
     
 }
 </script>
 
 <style>
+.contenedor{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
+}
+.form{
+    width: 340px;
+    height: auto;
+    background-color: #e6e6e6;
+    border-radius: 10px;
+    padding: 20px 30px;
+    font-family: 'Times New Roman', Times, serif;
+    box-shadow: 20px 20px 20px -10px;
+}
+h2{
+    margin: 10px 0;
+    padding-bottom: 20px;
+    width: 180px;
+    color: #585870;
+    border-bottom: 3px solid #78788c;
+}
+p:before{
+    content: attr(type);
+    display: block;
+    margin: 5px 2px;
+    font-family: 'Courier New', Courier, monospace;
+    color: #5a5a5a; 
+}
+input{
+    width: 100%;
+    border: 0;
+    border-bottom: 2px solid #bebed2;
+    background: none;
+}
+input:focus{
+    outline: none;
+    border-bottom: 2px solid #78788c;
+}
+#id_insertar{
+    width: 250 px;
+}
+#div_btn{
 
+   text-align: center;
+}
+.btn{
+    font-size: 20px;
+    border-radius: 7px;
+    border: groove;
+    margin: 30px 0;
+}
+.btn:hover{
+    background-color: rgb(188, 237, 255);
+    
+}
 </style>
--- src/helpers/clienteEstudiante.js
+++ src/helpers/clienteEstudiante.js
@@ -11,10 +11,40 @@ const incertar = async(body)=>{
     console.log(data);
 }
 
+/*
+const consultarTodos = async()=>{
+    const data = axios.get("http://localhost:8080/API/v1.0/Matricula/estudiantes10").then(ret => ret.data);
+    console.log(data);
+}
+
+const actualizar = async(body)=>{
+    const data = await axios.put("http://localhost:8080/API/v1.0/Matricula/estudiantes10/", body);
+    console.log(data);
+}
+
+
+const borrar = async(body)=>{
+    const data = axios.delete("http://localhost:8080/API/v1.0/Matricula/estudiantes10"+id).then(ret => ret.data);
+    console.log(data);
+}
+
+*/
+
 export const consultarFachada = async(id)=>{
     return await consultar(id);
 }
 
-export const incertarFachada = async(body)=>{
+export const insertarFachada = async(body)=>{
     await incertar(body);
 }
+/*
+export const consultarTodosFachada = async(id)=>{
+    return await consultarTodos();
+}
+export const actualizarFachada = async(body)=>{
+    return await actualizar(body);
+}
+export const borrarFachada = async(id)=>{
+    return await borrar(id);
+}
+*/
